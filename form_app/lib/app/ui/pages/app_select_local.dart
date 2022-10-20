24bdeb0e6bb9db28a5cbca7f4c0e3edbdb7b0567
Sergei <t.s.o.y.ser@mail.ru>
2022-10-21 00:18:59 +0600

    First commit
    First commit

---
+++ form_app/lib/app/ui/pages/app_select_local.dart
@@ -0,0 +1,32 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:form_app/translations/locale_keys.g.dart';
+
+class AppSelectPage extends StatefulWidget {
+  const AppSelectPage({Key? key}) : super(key: key);
+
+  @override
+  State<AppSelectPage> createState() => _AppSelectPageState();
+}
+
+class _AppSelectPageState extends State<AppSelectPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          ElevatedButton(onPressed: () async{
+            await context.setLocale(Locale('ru'));
+          }, child: Text("RU")),
+          ElevatedButton(onPressed: () async{
+            await context.setLocale(Locale('en'));
+          }, child: Text("EN")),
+          ElevatedButton(onPressed: () async{
+            await context.setLocale(Locale('kk'));
+          }, child: Text("KZ")),
+          Text(LocaleKeys.hello_text.tr())
+        ],
+      ),
+    );
+  }
+}
